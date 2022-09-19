6a0db4aa1b679d3252438f44a36e67aa454bd9c7
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-19 19:54:25 +0530

    Show the total amount of male patients and the total amount of female patients in the patients table. Display the two results in the same row.

---
+++ medium/gender_count.sql
@@ -0,0 +1,13 @@
+--Show the total amount of male patients and the total amount of female patients in the patients table.
+--Display the two results in the same row.
+select
+(select count(*) from patients where gender='M') as male_count,
+(select count(*) from patients where gender='F') as f_count
+
+OR
+
+select
+sum(gender='M') as male_count,
+sum(gender='F') as female_count
+from 
+patients
